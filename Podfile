ecb4d23a9d5ff5980a5d3ec6245cee8d2eab05de
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-04-30 22:49:00 +0300

    add APNS

--- Podfile
+++ Podfile
@@ -1,22 +1,18 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '11.0'
 
 target 'privacyIDEA Authenticator' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for privacyIDEA Authenticator
-pod 'SwiftOTP'
-pod 'KeychainSwift', '~>11.0'
+  pod 'SwiftOTP', '~> 1.0'
+  pod 'KeychainSwift', '~> 15.0'
+  pod 'Firebase', '~> 5.20.2'
+  pod 'Firebase/Messaging'
 
   target 'privacyIDEA AuthenticatorTests' do
     inherit! :search_paths
-    # Pods for testing
   end
 
   target 'privacyIDEA AuthenticatorUITests' do
     inherit! :search_paths
-    # Pods for testing
   end
-
 end
--- privacyIDEA Authenticator/AppDelegate.swift
+++ privacyIDEA Authenticator/AppDelegate.swift
@@ -4,47 +4,82 @@
 //
 //  Created by Nils Behlen on 07.08.18.
 //  Copyright © 2018 Nils Behlen. All rights reserved.
+//  Updated Vadim Zavjalov, Pharos Production Inc. on 30.04.19.
 //
 
 import UIKit
+import UserNotifications
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // Variables
+    
     var window: UIWindow?
     var tableVC: TableViewController?
-
+    
+    // Life
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupFirebase()
+        registerForPushNotifications()
+        
         return true
     }
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        setupApnsWithToken(deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         tableVC?.saveTokenlist()
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         tableVC?.saveTokenlist()
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         tableVC?.refreshTokenlist()
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         tableVC?.saveTokenlist()
     }
-
-
+    
+    // Private
+    
+    private func setupFirebase() {
+        FirebaseApp.configure()
+    }
+    
+    private func setupApnsWithToken(_ deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    private func registerForPushNotifications() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { [weak self] granted, error in
+            guard granted else { return }
+            self?.getNotificationSettings()
+        }
+    }
+    
+    private func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            guard settings.authorizationStatus == .authorized else { return }
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
 }
 
